37827dd5cac157b7f2e93686a1c86d814d2b6306
Felix <34609664+z-felixhan@users.noreply.github.com>
2022-01-27 22:18:04 -0500

    Fetch data

--- src/Fact.js
+++ src/Fact.js
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+
 const Fact = () => {
+  const url = "https://zoo-animal-api.herokuapp.com/animals/rand";
+
+  const fetchFacts = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchFacts();
+  }, []);
+
   return <h2>Fact Component</h2>;
 };
 
