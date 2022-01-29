1d451f8dfc139c2a2e623d2db711bad8ce0c3e9b
Felix <34609664+z-felixhan@users.noreply.github.com>
2022-01-28 22:27:21 -0500

    Structure fact

--- src/Fact.js
+++ src/Fact.js
@@ -1,20 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Fact = () => {
+  const [fact, setFact] = useState([]);
   const url = "https://zoo-animal-api.herokuapp.com/animals/rand";
 
   const fetchFacts = async () => {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    setFact(data);
   };
 
   useEffect(() => {
     fetchFacts();
   }, []);
 
-  return <h2>Fact Component</h2>;
+  return (
+    <article className="review">
+      <h4 className="author">{fact.name}</h4>
+      <p className="job">{fact.latin_name}</p>
+      <p className="info">{fact.animal_type}</p>
+    </article>
+  );
 };
 
 export default Fact;
