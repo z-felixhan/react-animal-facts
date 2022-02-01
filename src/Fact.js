c6581b6e9ca4a30ce78dff9bedde5bdede598a30
Felix <34609664+z-felixhan@users.noreply.github.com>
2022-01-31 21:47:49 -0500

    Add animal image

--- src/Fact.js
+++ src/Fact.js
@@ -15,10 +15,13 @@ const Fact = () => {
     fetchFacts();
   }, []);
 
-  const { name, latin_name, animal_type } = fact;
+  const { animal_type, image_link, latin_name, name } = fact;
 
   return (
     <article className="review">
+      <div className="img-container">
+        <img className="person-img" src={image_link} alt="" />
+      </div>
       <h4 className="author">{name}</h4>
       <p className="job">{latin_name}</p>
       <p className="info">{animal_type}</p>
